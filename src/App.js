b91989df71c224c87d4b2668d72abf4f6f716ef5
shazaib munawar <shazaib424647@gmail.com>
2022-08-09 20:16:37 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [acceptCookie, setAcceptCookie] = useState(
+        document.cookie.includes("status=accepted")
+            ? "accepted"
+            : "not accepted"
+    );
+    const [userIp, setUserIp] = useState("");
+
+    const language = window.navigator.language || window.navigator.userLanguage;
+    let device = "";
+    let browserName = "";
+    let dimentions = `${window.innerWidth}x${window.innerHeight}`;
+
+    const getIpAddress = async () => {
+        const response = await axios.get("https://api.ipify.org?format=json");
+        const userIp = await response.data;
+
+        setUserIp(userIp.ip);
+    };
+
+    useEffect(() => {
+        getIpAddress();
+    }, []);
+
+    //using media query to check mobile or desktop
+    if (window.matchMedia("(max-width: 767px)").matches) {
+        device = "mobile or tablet";
+    } else {
+        device = "desktop or laptop";
+    }
+
+    //using navigator to check browser version
+    if (window.navigator.userAgent.includes("Firefox/")) {
+        browserName = "firefox";
+    } else if (window.navigator.userAgent.includes("Edg/")) {
+        browserName = "edge";
+    } else if (window.navigator.userAgent.includes("Chrome/")) {
+        browserName = "chrome";
+    } else if (window.navigator.userAgent.includes("Safari/")) {
+        browserName = "safari";
+    }
+
+    const setCookie = () => {
+        document.cookie = "status=accepted";
+        setAcceptCookie("accepted");
+    };
+
+    return (
+        <>
+            <div className="App">
+                <h1>language: {language}</h1>
+                <h1>device: {device}</h1>
+                <h1>brower name: {browserName}</h1>
+                <h1>dimentions: {dimentions}</h1>
+                <h1>ip address: {userIp}</h1>
+                <h1>cookie status: {acceptCookie}</h1>
+            </div>
+            {acceptCookie !== "accepted" ? (
+                <div>
+                    <h1>cookies</h1>
+                    <button onClick={setCookie}>accept</button>
+                </div>
+            ) : (
+                ""
+            )}
+        </>
+    );
 }
 
 export default App;
